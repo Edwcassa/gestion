de758237c194066154b1186958464904144c90f1
Edwcassa <edwar.cassa@gmail.com>
2022-09-25 19:58:17 -0500

    add cambiar imagen perfil usuario

--- src/pages/views/Perfil.jsx
+++ src/pages/views/Perfil.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
 import Swal from 'sweetalert2'
 import UseAuth from '../../auth/UseAuth'
 import { useForm } from '../../hooks/useForm'
@@ -10,12 +11,32 @@ export default function Perfil() {
   const [showPassword, setShowPassword] = useState(false)
   const [values, handleInputChange] = useForm()
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  // const [openModal, setOpenModal] = useState(false)
+  // Estados para el modal
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  //
+
+  //estado para guardar la image seleccionada
+  const [imgSelected, setImgSelected] = useState(null)
+  const [updateImgLoad, setUpdateImgLoad] = useState(false)
+
+  //imagenes
+  const imagesProfile = [
+    { 'img': 'https://i.postimg.cc/zfc2wwJP/descargar-2-1.png' },
+    { 'img': 'https://i.postimg.cc/y6QtXSPR/aaaa-1.png' },
+    { 'img': 'https://i.postimg.cc/zDCZNMDz/ZEPETO-2.png' },
+    { 'img': 'https://i.postimg.cc/FHK2jHyz/descargar-3.png' },
+    { 'img': 'https://i.postimg.cc/xT5wJzbB/Dv-WFXNp-Vs-AAez-KJ-1.png' }
+  ]
+
+  // datos del usuario
   const { user, setUser } = UseAuth()
 
+  // d
   const {
     email, nombre, apellido, categoria, sexo,
     telefono, codDocente, img, contrasenia, _id } = user || {}
@@ -26,7 +47,7 @@ export default function Perfil() {
     axios
       .put(`/docentes/${_id}`, values)
       .then((response) => {
-        console.log(response.data)
+        // console.log(response.data)
         if (response.data.ok) {
           const newUserData = { ...user, ...values }
           setUser(newUserData)
@@ -49,6 +70,41 @@ export default function Perfil() {
       .finally(() => setLoading(false));
   }
 
+  const updateImg = async() => {
+    if (!imgSelected) {
+      Swal.fire('Selecciona una imagen')
+      return
+    }
+    setUpdateImgLoad(true)
+    await axios
+      .put(`/docentes/${_id}`, { 'img': imgSelected })
+      .then((response) => {
+        // console.log(response.data)
+        if (response.data.ok) {
+          const newUserData = { ...user, 'img': imgSelected, }
+          setUser(newUserData)
+          setUserLocal(newUserData)
+          Swal.fire(
+            'Imagen Actualizado!',
+            '¡Haz clic en el botón!',
+            'success'
+          )
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Algo salió mal!',
+          footer: `${error.message}`
+        })
+      })
+      .finally(() => setUpdateImgLoad(false))
+    //cerrar el modal
+    handleClose()
+  }
+
+
   return (
     <div style={{ "overflow": "hidden" }}>
       {/* <pre>{JSON.stringify(values, undefined, 2)}</pre> */}
@@ -57,7 +113,7 @@ export default function Perfil() {
           <div className="card">
             <div className="card-body">
               <div className="d-flex flex-column align-items-center text-center">
-                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150" />
+                <img src={img??"https://bootdey.com/img/Content/avatar/avatar7.png"} alt="Admin" className="rounded" width="150" />
                 <div className="mt-3">
                   <h4>{nombre ?? ''}</h4>
                   <span className="text-muted font-size-sm">{codDocente ?? ''}</span>
@@ -67,6 +123,7 @@ export default function Perfil() {
                 </div>
                 <button type="button"
                   className="btn btn-success px-4"
+                  onClick={handleShow}
                 >
                   Cambiar Foto
                 </button>
@@ -180,6 +237,43 @@ export default function Perfil() {
         </div>
       </div>
 
-    </div>
+
+      <Modal size="lg" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Elige una foto</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <pre>{JSON.stringify(imgSelected, undefined, 2)}</pre>
+          <div className='d-flex flex-row bd-highlight mb-3 justify-content-center'>
+            {
+              imagesProfile.map((e, i) => (
+                <div className="form-check" key={i}>
+                  <input className="form-check-input d-none" id={`radio${i}`} type="radio" name="img"
+                    value={e.img}
+                    onChange={(e) => setImgSelected(e.target.value)}
+                    checked={imgSelected === e.img} />
+                  <label className="form-check-label" htmlFor={`radio${i}`}>
+                    <img width={150}
+                      src={e.img} alt=""
+                      className={`image_hover img-fluid rounded border border-bottom border-4 ${e.img === imgSelected && 'border-success'}`} />
+                  </label>
+                </div>
+              ))
+            }
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className='btn btn-secondary' onClick={handleClose}>
+            Cerrar
+          </button>
+          <button className='btn btn-primary' onClick={()=>updateImg()} disabled={updateImgLoad}>
+            {updateImgLoad && <i className='fa fa-spinner fa-spin me-2 '></i>}
+            {updateImgLoad ? 'Espere...' : 'Actualizar Imagen'}
+          </button>
+        </Modal.Footer>
+      </Modal>
+
+
+    </div >
   )
 }
